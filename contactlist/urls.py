1f7ed46ea556949b41905fce96d371078d8584ad
orkarma <orkarma@me.com>
2017-08-23 11:14:13 +0100

    search funtion now working fully for first, last name and department

--- contactlist/urls.py
+++ contactlist/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     #url(r'^$', include('contacts.urls')),
     url(r'^contacts/', include('contacts.urls')),
     url(r'^admin/', admin.site.urls),
+    #url(r'^search/', include('search.urls'))
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -2,13 +2,22 @@
 #from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
 from .models import Contact
 
 # Create your views here.
 def index(request):
     #return HttpResponse('hello world')
+
     contacts = Contact.objects.all()[:10]
+    query = request.GET.get("q")
+    if query:
+        contacts = Contact.objects.filter(
+            Q(Name__icontains=query) |
+            Q(Department__icontains=query)
+            )
+    #contacts = Contact.objects.filter(Name__icontains="")
     context = {
         'contacts':contacts
     }
